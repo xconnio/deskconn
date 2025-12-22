43ca4a46baaf38f9ed171dc8b1fceb3bc32d956a
Muzzammil Shahid <muzzammil@xconn.io>
2025-12-22 19:22:17 +0500

    implement shell

--- cmd/deskconnctl/main.go
+++ cmd/deskconnctl/main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/xconnio/deskconn"
+	"github.com/xconnio/xconn-go"
 )
 
 func main() {
@@ -18,6 +19,10 @@ func main() {
 	switch os.Args[1] {
 	case "attach":
 		if err := attach(os.Args[2:]); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	case "shell":
+		if err := shell(os.Args[2:]); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 		}
 	default:
@@ -51,6 +56,38 @@ func attach(args []string) error {
 		deviceName = host
 	}
 	return deskconn.Attach(context.Background(), username, password, deviceName)
+}
+
+func shell(args []string) error {
+	if len(args) != 2 {
+		return fmt.Errorf("shell requires <username> <password>")
+	}
+	username := args[0]
+	password := args[1]
+	session, err := xconn.ConnectCRA(context.Background(), deskconn.URI, deskconn.Realm, username, password)
+	if err != nil {
+		return err
+	}
+
+	callResp := session.Call("io.xconn.deskconn.desktop.list").Do()
+	if callResp.Err != nil {
+		return callResp.Err
+	}
+	if len(callResp.Args()) == 0 {
+		return fmt.Errorf("no desktop attached to the account")
+	}
+
+	deviceDict, err := callResp.ArgDict(0)
+	if err != nil {
+		return err
+	}
+
+	machineID, err := deviceDict.String("authid")
+	if err != nil {
+		return err
+	}
+
+	return deskconn.StartInteractiveShell(session, fmt.Sprintf(deskconn.ProcedureShellCloud, machineID))
 }
 
 func usage() {
